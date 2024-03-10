6e8c6864fd828faf236faf5b17783026c5e74b0e
Ainur Iskhakov <aiinuur2314@gmail.com>
2024-03-10 17:07:54 +0300

    Added gitignore

--- UserService/UserService.Database/obj/Debug/net8.0/UserService.Persistance.AssemblyInfo.cs
+++ UserService/UserService.Database/obj/Debug/net8.0/UserService.Persistance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Persistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8501d6bacb6a4e26b99ca8813139ef1c503cde52")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Persistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Persistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UserService/Web/obj/Debug/net8.0/UserService.Web.AssemblyInfo.cs
+++ UserService/Web/obj/Debug/net8.0/UserService.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8501d6bacb6a4e26b99ca8813139ef1c503cde52")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
