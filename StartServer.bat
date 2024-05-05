f19e2199fe380974abeb0e10585992428e57d023
Ainur Iskhakov <aiinuur2314@gmail.com>
2024-05-05 23:34:23 +0300

    Added all server to bat start script.

--- StartServer.bat
+++ StartServer.bat
@@ -1,2 +1,7 @@
 start dotnet run --project UserService\Web\UserService.Web.csproj
 start dotnet run --project EmployeeService\EmployeeService.API\EmployeeService.API.csproj
+start dotnet run --project EmailService\EmailService\EmailService.csproj
+start MediaService\media_server.exe
+start dotnet run --project RecruitmentService\RecruitmentService.Web\RecruitmentService.Web.csproj
+start dotnet run --project ScrapersService\ScrapersService.Main\ScrapersService.Main.csproj
+start dotnet run --project StudyService\StudyService.Web\StudyService.Web.csproj
